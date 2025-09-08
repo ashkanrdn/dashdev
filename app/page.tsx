34902b080db57badf360906dc8ce2d52ba9a4b0b
Ashkan Radnia <41102393+ashkanrdn@users.noreply.github.com>
2025-09-07 22:04:39 -0400

    .

--- app/page.tsx
+++ app/page.tsx
@@ -910,7 +910,7 @@ export default function Home() {
                       <rect width="20" height="16" x="2" y="4" rx="2"></rect>
                       <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
                     </svg>
-                    <span className="text-muted-foreground">info@dash-dev.com</span>
+                    <span className="text-muted-foreground">info@radnia.studio</span>
                   </li>
                   <li className="flex items-start gap-2">
                     <svg
